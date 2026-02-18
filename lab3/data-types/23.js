94414d161cd29a596d8386310cee8460409dcc4f
mkhtassema <a_mukhtarkyzy@kbtu.kz>
2026-02-18 16:28:03 +0500

    I accidentally deleted the projects from the GitHub, however, they had been uploaded earlier in a timely manner

---
+++ lab3/data-types/23.js
@@ -0,0 +1,10 @@
+function getSecondsToday() {
+  let now = new Date();
+
+  let today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+
+  let diff = now - today; // ms difference
+  return Math.round(diff / 1000); // make seconds
+}
+
+alert( getSecondsToday() );
